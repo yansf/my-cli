5ff8480996ba3f9f8cd50982944785759846cfb8
yansf <y836581091@163.com>
2019-09-20 16:57:58 +0800

    'chang'

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const gulp-rollup = require('gulp-rollup');
+
+//开发环境
+gulp.task("builddev",()=>{
+    
+})
+
+
+//上线环境
+gulp.task("buildprod",()=>{
+
+})
+
+// 测试环境 
+gulp.task('')
